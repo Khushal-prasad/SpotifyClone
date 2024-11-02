4f333770c181d96ebcb2eee281f82ca7fa6fa2b7
Khushal-prasad <jeetuprasad277@gmail.com>
2024-11-02 18:01:54 +0530

    first commit

---
+++ src/Components/Explore.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Explore = () => {
+    return (
+        <>
+        <Navbar/>
+        <div className='bg-red-600'>
+        </div>
+        </>
+    )
+}
+
+export default Explore
