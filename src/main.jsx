0b81899a61419aac9cad2206e5a92c6e92fa3766
Khushal-prasad <jeetuprasad277@gmail.com>
2025-04-11 10:41:30 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './global.css'
+import './Global.css'
 import { BrowserRouter } from 'react-router-dom'
 import PlayerContextProvider from './Context/PlayerContext.jsx'
 
